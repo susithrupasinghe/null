b325c3105ee1d0f2d19a4cbb1cf63e163e951544
Susith Rupasinghe <susith03program@gmail.com>
2022-09-07 01:36:40 +0530

    basic added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Landing from './common/header/headerComponent';
+import EmployeeLoginPage from './pages/Employee/EmployeeLoginPage/EmployeeLoginPage';
+import AdminLoginPage from './pages/Admin/AdminLoginPage/AdminLoginPage';
+import PMLoginPage from './pages/ProjectManager/ProjectManagerLoginPage/ProjectManagerLoginPage';
+import AdminDashboardPage from "./pages/Admin/AdminDashboard/AdminDashboard";
+import EmployeeDashboard from "./pages/Employee/EmployeeDashboard/EmployeeDashboard";
+import ProjectManagerDashboard from "./pages/ProjectManager/ProjectManagerDashboard/ProjectManageDashboard";
+import ProjectManagementDashboardPage from "./pages/ProjectManagement/ProjectManagementDashboard/ProjectManagementDashboard";
+import "./text.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Landing/>
+      <Routes>
+        <Route excat path="/emplogin" element={<EmployeeLoginPage />} />
+        <Route excat path="/adminlogin" element={<AdminLoginPage />} />
+        <Route excat path="/pmlogin" element={<PMLoginPage />} />
+
+
+        <Route excat path="/admin/dashboard" element={<AdminDashboardPage />} />
+        <Route excat path="/employee/dashboard" element={<EmployeeDashboard />} />
+        <Route excat path="/pm/dashboard" element={<ProjectManagerDashboard />} />
+        <Route excat path="/management/dashboard" element={<ProjectManagementDashboardPage />} />
+      </Routes>
+    </Router>
+
+
+
+
+
   );
 }
 
